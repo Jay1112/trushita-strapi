ad7cf54154e38eda2c1b284ddee9a84ed5d0f33a
Jay Patel <jaypatel@Jays-MacBook-Pro.local>
2026-05-01 22:21:53 +0530

    fixes of db

--- config/database.js
+++ config/database.js
@@ -1,16 +1,19 @@
-const path = require('path');
-
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
       host: env("DATABASE_HOST"),
-      port: env("DATABASE_PORT"),
+      port: env.int("DATABASE_PORT"),
       database: env("DATABASE_NAME"),
       user: env("DATABASE_USERNAME"),
       password: env("DATABASE_PASSWORD"),
-      ssl: env.bool("DATABASE_SSL"),
+      ssl: {
+        rejectUnauthorized: false,
+      },
     },
-    useNullAsDefault: true
-  }
-})
+    pool: {
+      min: 2,
+      max: 10,
+    },
+  },
+});

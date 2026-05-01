95de987b89184ea35117eadca8d670a69760b7dd
Jay Patel <jaypatel@Jays-MacBook-Pro.local>
2026-05-01 22:59:42 +0530

    db fix

--- config/database.js
+++ config/database.js
@@ -1,20 +1,32 @@
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
-    connection: {
-      host: env("DATABASE_HOST"),
-      port: env.int("DATABASE_PORT"),
-      family: 4,
-      database: env("DATABASE_NAME"),
-      user: env("DATABASE_USERNAME"),
-      password: env("DATABASE_PASSWORD"),
+module.exports = ({ env }) => {
+  const client = 'postgres';
+
+  const connection = env('DATABASE_URL')
+    ? {
+      connectionString: env('DATABASE_URL'),
       ssl: {
-        rejectUnauthorized: false,
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF_SIGNED', false),
+      },
+    }
+    : {
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'strapi'),
+      user: env('DATABASE_USERNAME', 'strapi'),
+      password: env('DATABASE_PASSWORD', 'strapi'),
+      ssl: env.bool('DATABASE_SSL', true)
+        ? { rejectUnauthorized: env.bool('DATABASE_SSL_SELF_SIGNED', false) }
+        : false,
+    };
+
+  return {
+    connection: {
+      client,
+      connection,
+      pool: {
+        min: env.int('DATABASE_POOL_MIN', 2),
+        max: env.int('DATABASE_POOL_MAX', 10),
       },
     },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-  },
-});
+  };
+};

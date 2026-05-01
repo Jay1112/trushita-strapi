b215bbdbbfc7213d75954e0debc64835af13a433
Jay Patel <jaypatel@Jays-MacBook-Pro.local>
2026-05-01 23:29:16 +0530

    db connection

--- src/admin/vite.config.example.js
+++ src/admin/vite.config.example.js
@@ -1,12 +1,14 @@
 const { mergeConfig } = require('vite');
 
 module.exports = (config) => {
-  // Important: always return the modified config
   return mergeConfig(config, {
     resolve: {
       alias: {
         '@': '/src',
       },
+    },
+    server: {
+      allowedHosts: ['trushita-strapi.onrender.com'],
     },
   });
 };
